1f54c818cd378cebd96aae3860d7431509c1e436
HoonyHoony <dynamic97312@naver.com>
2023-11-17 14:42:35 +0900

    update2

--- src/sbadmin/urls.py
+++ src/sbadmin/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -28,3 +30,6 @@ urlpatterns = [
 
     path('users/', include('users.urls', namespace="users")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
